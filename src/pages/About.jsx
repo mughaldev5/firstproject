c036856727f8ef3b1761c1019ae829411874f54a
mughaldev5 <ahmadmughal9274@gmai.com>
2025-08-11 01:09:45 -0700

    about page created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Layout from '../Wrapper/Layout'
 
 const About = () => {
   return (
-    
-    <Layout>
     <div>
-      <h1>This is About</h1>
+      <h1>here is about page</h1>
     </div>
-      </Layout>
-    
   )
 }
 
